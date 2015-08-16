6d39435b06ff743ffea14d6f26fb6ca9ff11f5a3
gariken <schet941@gmail.com>
2015-08-16 03:50:42 +0300

    fact

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
-<?php 
-echo "class!!";
+<?php
+class Factorial
+{
+    public function Fact($n)
+    {
+        $fact = 1;
+        for($i=2; $i<=$n; $i++)
+        {
+            $fact*=$i;
+        }
+        return $fact;
+    }
+}
+
+$new = new Factorial();
+
+echo $new ->Fact(5);
 
